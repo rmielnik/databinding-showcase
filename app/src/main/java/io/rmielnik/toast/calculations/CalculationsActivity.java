a29f3a47e396ba51b87b34e3dc1201f674dc3a66
Robert Mielnik <rm2cdr@gmail.com>
2017-07-28 17:29:41 +0200

    CalculationsViewModel cleanup

--- app/src/main/java/io/rmielnik/toast/calculations/CalculationsActivity.java
+++ app/src/main/java/io/rmielnik/toast/calculations/CalculationsActivity.java
@@ -6,24 +6,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.jakewharton.rxbinding2.view.RxView;
-
-import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
-import io.reactivex.ObservableTransformer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
-import io.reactivex.observers.DisposableObserver;
-import io.reactivex.schedulers.Schedulers;
 import io.rmielnik.toast.R;
-import io.rmielnik.toast.calculations.request.AServiceRequest;
-import io.rmielnik.toast.calculations.request.BServiceRequest;
-import io.rmielnik.toast.calculations.request.Request;
-import io.rmielnik.toast.calculations.response.AResponse;
-import io.rmielnik.toast.calculations.response.BResponse;
-import io.rmielnik.toast.calculations.response.Response;
 import io.rmielnik.toast.databinding.ActivityExampleCalculationsBinding;
 
 public class CalculationsActivity extends AppCompatActivity {
@@ -36,99 +19,16 @@ public class CalculationsActivity extends AppCompatActivity {
         return intent;
     }
 
-    private CalcService aService = new CalcService();
-    private CalcService bService = new CalcService();
-
     private ActivityExampleCalculationsBinding binding;
     private CalculationsViewModel model = new CalculationsViewModel();
-    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initModel();
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_example_calculations);
-        binding.setModel(model);
-
-        final ObservableTransformer<AServiceRequest, AResponse> consumeA =
-                stream -> stream
-                        .observeOn(Schedulers.io())
-                        .flatMap(new Function<AServiceRequest, ObservableSource<AResponse>>() {
-                            @Override
-                            public ObservableSource<AResponse> apply(@NonNull AServiceRequest aServiceRequest) throws Exception {
-                                return Observable.fromCallable(
-                                        () -> AResponse.success(aService.randomSleep()))
-                                        .startWith(AResponse.inProgress());
-                            }
-                        });
-
-        final ObservableTransformer<BServiceRequest, BResponse> consumeB =
-                stream -> stream
-                        .observeOn(Schedulers.io())
-                        .flatMap(new Function<BServiceRequest, ObservableSource<BResponse>>() {
-                            @Override
-                            public ObservableSource<BResponse> apply(@NonNull BServiceRequest bServiceRequest) throws Exception {
-                                return Observable.fromCallable(
-                                        () -> BResponse.success(bService.randomSleep()))
-                                        .startWith(BResponse.inProgress());
-                            }
-                        });
-
-        final ObservableTransformer<Request, Response> splitAndMerge =
-                events -> events.publish(shared -> Observable.merge(
-                        shared.ofType(AServiceRequest.class).compose(consumeA),
-                        shared.ofType(BServiceRequest.class).compose(consumeB)
-                ));
-
-        disposable = RxView.clicks(binding.run)
-                .observeOn(Schedulers.computation())
-                .flatMap(new Function<Object, ObservableSource<Request>>() {
-                    @Override
-                    public ObservableSource<Request> apply(@NonNull Object o) throws Exception {
-                        return Observable.fromArray(new AServiceRequest(), new BServiceRequest());
-                    }
-                })
-                .compose(splitAndMerge)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(consumeMagicNumbers());
-    }
-
-    private void initModel() {
         String greeting = getIntent().getStringExtra(EXTRA_GREETING);
         model.greeting.set(greeting);
-    }
 
-    @android.support.annotation.NonNull
-    private DisposableObserver<Response> consumeMagicNumbers() {
-        return new DisposableObserver<Response>() {
-            @Override
-            public void onNext(@NonNull Response response) {
-                boolean inProgress = response.isProgress();
-                int result = response.getResult();
-
-                if (response instanceof AResponse) {
-                    model.aResult.set(inProgress ? "In progress" : "A request executed in: " + result + "ms");
-                }
-                if (response instanceof BResponse) {
-                    model.bResult.set(inProgress ? "In progress" : "B request executed in: " + result + "ms");
-                }
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-            }
-
-            @Override
-            public void onComplete() {
-            }
-        };
-    }
-
-    @Override
-    protected void onDestroy() {
-        if (disposable != null && !disposable.isDisposed()) {
-            disposable.dispose();
-        }
-        super.onDestroy();
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_example_calculations);
+        binding.setModel(model);
     }
 }
--- app/src/main/java/io/rmielnik/toast/calculations/CalculationsViewModel.java
+++ app/src/main/java/io/rmielnik/toast/calculations/CalculationsViewModel.java
@@ -2,10 +2,100 @@ package io.rmielnik.toast.calculations;
 
 import android.databinding.ObservableField;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+import io.rmielnik.toast.calculations.request.AServiceRequest;
+import io.rmielnik.toast.calculations.request.BServiceRequest;
+import io.rmielnik.toast.calculations.request.Request;
+import io.rmielnik.toast.calculations.response.AResponse;
+import io.rmielnik.toast.calculations.response.BResponse;
+import io.rmielnik.toast.calculations.response.Response;
+
 public class CalculationsViewModel {
 
     public final ObservableField<String> aResult = new ObservableField<>("Idle");
     public final ObservableField<String> bResult = new ObservableField<>("Idle");
     public final ObservableField<String> greeting = new ObservableField<>();
 
+    private ObservableTransformer<Request, Response> computationsHandler;
+    private Disposable ongoingRequest;
+
+    private CalcService aService = new CalcService();
+    private CalcService bService = new CalcService();
+
+    public CalculationsViewModel() {
+        final ObservableTransformer<AServiceRequest, AResponse> consumeA =
+                stream -> stream
+                        .observeOn(Schedulers.io())
+                        .flatMap(new Function<AServiceRequest, ObservableSource<AResponse>>() {
+                            @Override
+                            public ObservableSource<AResponse> apply(@NonNull AServiceRequest aServiceRequest) throws Exception {
+                                return Observable.fromCallable(
+                                        () -> AResponse.success(aService.randomSleep()))
+                                        .startWith(AResponse.inProgress());
+                            }
+                        });
+
+        final ObservableTransformer<BServiceRequest, BResponse> consumeB =
+                stream -> stream
+                        .observeOn(Schedulers.io())
+                        .flatMap(new Function<BServiceRequest, ObservableSource<BResponse>>() {
+                            @Override
+                            public ObservableSource<BResponse> apply(@NonNull BServiceRequest bServiceRequest) throws Exception {
+                                return Observable.fromCallable(
+                                        () -> BResponse.success(bService.randomSleep()))
+                                        .startWith(BResponse.inProgress());
+                            }
+                        });
+
+        computationsHandler =
+                events -> events.publish(shared -> Observable.merge(
+                        shared.ofType(AServiceRequest.class).compose(consumeA),
+                        shared.ofType(BServiceRequest.class).compose(consumeB)
+                ));
+    }
+
+    public void calculate() {
+        if (ongoingRequest != null && !ongoingRequest.isDisposed()) {
+            ongoingRequest.dispose();
+        }
+        ongoingRequest = Observable.fromArray(new AServiceRequest(), new BServiceRequest())
+                .observeOn(Schedulers.computation())
+                .compose(computationsHandler)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(consumeMagicNumbers());
+    }
+
+    @android.support.annotation.NonNull
+    private DisposableObserver<Response> consumeMagicNumbers() {
+        return new DisposableObserver<Response>() {
+            @Override
+            public void onNext(@NonNull Response response) {
+                boolean inProgress = response.isProgress();
+                int result = response.getResult();
+
+                if (response instanceof AResponse) {
+                    aResult.set(inProgress ? "In progress" : "A request executed in: " + result + "ms");
+                }
+                if (response instanceof BResponse) {
+                    bResult.set(inProgress ? "In progress" : "B request executed in: " + result + "ms");
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        };
+    }
 }
